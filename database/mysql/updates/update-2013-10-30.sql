4e031b126ea4f568188343ccba7db59368510007
Annie Zhou <anniezhou91@gmail.com>
2013-10-31 12:13:56 -0400

    change admin_lab_config to _admin.labConfig for convention rule (last commit overwrote Lingmin's commit, put it back)

---
+++ database/mysql/updates/update-2013-10-30.sql
@@ -0,0 +1,2 @@
+UPDATE secObjectName SET objectName='_admin.labConfig' WHERE objectName='admin_lab_config';
+UPDATE secObjPrivilege SET objectName='_admin.labConfig' WHERE objectName='admin_lab_config';
